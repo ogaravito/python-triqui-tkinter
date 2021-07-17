16c38cb64b8e830882bdfea8dcb80d5ca0d2ed1d
Olger Elias Garavito Muñoz <ogaravito@pedagogica.edu.co>
2021-07-17 18:12:14 -0500

    Ejemplo para los estudiante del grupo de Python

--- triqui.py
+++ triqui.py
@@ -2,19 +2,21 @@ from tkinter import *
 from tkinter import messagebox
 
 matriz = [  ['' for j in range(3) ]  for i in range(3) ]
-triqui = [['X','X','X'],['O','O','O']]
+triqui = [   ['X','X','X']   ,  ['O','O','O']    ]
 lista_botones = []
 ultima_letra = ''
 
-def limpiar():
+def limpiar(gano):
 	global matriz
 	global ultima_letra
-	messagebox.showinfo('TRIQUI', "Has ganado\n" + ultima_letra)
+	if gano:
+		messagebox.showinfo('TRIQUI', "Has ganado\n" + ultima_letra)
 	matriz = [  ['' for j in range(3) ]  for i in range(3) ]
 	for i in lista_botones:
 		i['text']=''
 
 def clic_btn(num_btn):
+	print(num_btn)
 	global ultima_letra
 	k=0
 	for i in range(3):
@@ -26,13 +28,36 @@ def clic_btn(num_btn):
 					btn['text'] = ultima_letra
 			k+=1
 
+	for i in range(3):
+		if matriz[i] in triqui:
+			print("hiciste triqui h")
+
+	for i in range(3):
+		columna = []
+		for j in range(3):
+			columna.append(matriz[j][i])
+		if columna in triqui: 
+			print("Hiciste triqui v")
+
+	diagonal = []
+	for i in range(3):
+		diagonal.append(matriz[i][i])
+	if diagonal in triqui:
+		print("Hiciste triqui d")
+
+	diagonal2 = []
+	for i in range(3):
+		diagonal2.append(matriz[i][2-i])
+	if diagonal2 in triqui:
+		print("Hiciste triqui d2")
+
 	for i in range(3): 
 		if matriz[i] in triqui or [matriz[j][i] for j in range(3)] in triqui :
-			limpiar()
+			limpiar(True)
 			return
 
 	if [ matriz[i][i]   for i in range(3)] in triqui or [ matriz[i][2-i]   for i in range(3)] in triqui:
-		limpiar()
+		limpiar(True)
 		return
 
 ventana = Tk()
@@ -46,4 +71,14 @@ for i in range(3):
 		lista_botones.append(boton)
 		k+=1
 
+btn_limpiar = Button(ventana, text=" Iniciar ", command=lambda:limpiar(False), height=3, bg="#999", fg="#fff")
+btn_limpiar.grid(row=3, column=0, columnspan=3, sticky="nsew")
+
+
+
 ventana.mainloop()
+
+
+
+
+
